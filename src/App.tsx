367693db0d981e35385790ae794d683747c5baad
tpantsar <tomi.pantsar@gmail.com>
2025-03-28 15:02:37 +0200

    format with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import { Button, Divider, Container, Typography } from '@mui/material';
+import { Button, Container, Divider, Typography } from '@mui/material';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
-import { apiBaseUrl } from "./constants";
-import { Patient } from "./types";
+import { apiBaseUrl } from './constants';
+import { Patient } from './types';
 
-import patientService from "./services/patients";
-import PatientListPage from "./components/PatientListPage";
+import PatientListPage from './components/PatientListPage';
+import patientService from './services/patients';
 
 const App = () => {
   const [patients, setPatients] = useState<Patient[]>([]);
@@ -21,12 +21,12 @@ const App = () => {
     };
     void fetchPatientList();
   }, []);
-  
+
   return (
     <div className="App">
       <Router>
         <Container>
-          <Typography variant="h3" style={{ marginBottom: "0.5em" }}>
+          <Typography variant="h3" style={{ marginBottom: '0.5em' }}>
             Patientor
           </Typography>
           <Button component={Link} to="/" variant="contained" color="primary">
@@ -34,7 +34,10 @@ const App = () => {
           </Button>
           <Divider hidden />
           <Routes>
-            <Route path="/" element={<PatientListPage patients={patients} setPatients={setPatients} />} />
+            <Route
+              path="/"
+              element={<PatientListPage patients={patients} setPatients={setPatients} />}
+            />
           </Routes>
         </Container>
       </Router>
